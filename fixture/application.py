d38138ab9dbd5e40e0c76b9e61459465f3244354
aquamuerta <aquamuerta@gmail.com>
2018-10-10 12:06:42 +0300

    contact.py перенесен в пакет model

--- fixture/application.py
+++ fixture/application.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
 from fixture.session import SessionHelper
 from fixture.group import GroupHelper
-from fixture.contact import ContactHelper
+from model.contact import ContactHelper
 
 class Application:
     def __init__(self):
