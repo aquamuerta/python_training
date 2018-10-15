5520dfebf2206469a9278b12dd2bc38b708a7c75
aquamuerta <aquamuerta@gmail.com>
2018-10-15 16:33:12 +0300

    Добавлено предусловие для теста модификации контакта.

--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -2,5 +2,7 @@ from model.contact import Contact
 
 
 def test_modify_contact_name(app):
+    if app.contact.count() == 0:
+        app.contact.create(Contact(name="test1"))
     app.contact.modify_first_contact(Contact(name="Alexander"))
     app.contact.return_to_home_page()
