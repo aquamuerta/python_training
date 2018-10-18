25f5a23dacd321ca555e42c601013a7b5058aa65
aquamuerta <aquamuerta@gmail.com>
2018-10-18 12:09:29 +0300

    Добавлена проверка длинны списка групп для теста удаления групп

--- test/test_del_group.py
+++ test/test_del_group.py
@@ -5,5 +5,8 @@ def test_delete_first_group(app):
     if app.group.count() == 0:
         app.group.create(Group(name="test"))
     app.group.open_groups_page()
+    old_groups = app.group.get_group_list()
     app.group.delete_first_group()
     app.group.return_to_home()
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) - 1 == len(new_groups)
