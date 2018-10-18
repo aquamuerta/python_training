27ac8bc778bfa4b99e9e7a2b25427831c743d3d6
aquamuerta <aquamuerta@gmail.com>
2018-10-18 12:13:05 +0300

    Добавлена проверка длинны списка групп для теста модификации групп

--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -4,14 +4,20 @@ from model.group import Group
 def test_modify_group_name(app):
     if app.group.count() == 0:
         app.group.create(Group(name="test"))
+    old_groups = app.group.get_group_list()
     app.group.open_groups_page()
     app.group.modify_first_group(Group(name="New group"))
     app.contact.return_to_home()
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
 
 
 def test_modify_group_header(app):
     if app.group.count() == 0:
         app.group.create(Group(header="test"))
+    old_groups = app.group.get_group_list()
     app.group.open_groups_page()
     app.group.modify_first_group(Group(header="New header"))
     app.contact.return_to_home()
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
