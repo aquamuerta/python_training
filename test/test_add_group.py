869b2e38d4fdb1f46eb772cd67ed6ed88d842b0b
aquamuerta <aquamuerta@gmail.com>
2018-10-18 12:02:29 +0300

    Добавлена проверка длинны списка групп для теста добавления групп

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -4,7 +4,9 @@ from model.group import Group
 
 
 def test_add_group(app):
+        old_groups = app.group.get_group_list()
         app.group.open_groups_page()
         app.group.create(Group(name="gdfhjtyr", header="werthnm", footer="etthjyure"))
         app.group.return_to_groups_page()
-
+        new_groups = app.group.get_group_list()
+        assert len(old_groups) + 1 == len(new_groups)
