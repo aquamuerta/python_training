5ee456a7cc53f55fc7343cdfdc83ca7aaf1435f6
aquamuerta <aquamuerta@gmail.com>
2018-10-30 17:41:34 +0300

    Добавлена проверка длимнны списка контактов в тестах удаления и модификации контактов

--- test/test_del_contact.py
+++ test/test_del_contact.py
@@ -4,6 +4,9 @@ from model.contact import Contact
 def test_delete_first_contact(app):
     if app.contact.count() == 0:
         app.contact.create(Contact(name="test2"))
+    old_contacts = app.contact.get_contact_list()
     app.open_home_page()
     app.contact.delete_first_contact()
     app.contact.return_to_home()
+    new_contacts = app.contact.get_contact_list()
+    assert len(old_contacts) - 1 == len(new_contacts)
--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -4,5 +4,8 @@ from model.contact import Contact
 def test_modify_contact_name(app):
     if app.contact.count() == 0:
         app.contact.create(Contact(name="test1"))
+    old_contacts = app.contact.get_contact_list()
     app.contact.modify_first_contact(Contact(name="Alexander"))
     app.contact.return_to_home_page()
+    new_contacts = app.contact.get_contact_list()
+    assert len(old_contacts) == len(new_contacts)
