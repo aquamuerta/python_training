0b2caf3f19a5fedac082e25a33ca0dac99e902c6
aquamuerta <aquamuerta@gmail.com>
2018-12-26 10:40:48 +0300

    Реализовано кеширование для метода загрузки групп.

--- fixture/group.py
+++ fixture/group.py
@@ -21,6 +21,7 @@ class GroupHelper:
         self.fill_group_form(group)
         # submit group creation
         wd.find_element_by_name("submit").click()
+        self.group_cache = None
 
     def fill_group_form(self, group):
         wd = self.app.wd
@@ -50,6 +51,7 @@ class GroupHelper:
         self.fill_group_form(new_group_data)
         # submit modification
         wd.find_element_by_name("update").click()
+        self.group_cache = None
 
     def delete_first_group(self):
         wd = self.app.wd
@@ -58,6 +60,7 @@ class GroupHelper:
         # submit deletion
         wd.find_element_by_name("delete").click()
         wd.find_element_by_link_text("group page").click()
+        self.group_cache = None
 
     def select_first_group(self):
         wd = self.app.wd
@@ -69,12 +72,15 @@ class GroupHelper:
         self.open_groups_page()
         return len(wd.find_elements_by_name("selected[]"))
 
+    group_cache = None
+
     def get_group_list(self):
-        wd = self.app.wd
-        self.open_groups_page()
-        groups = []
-        for element in wd.find_elements_by_css_selector("span.group"):
-            text = element.text
-            id = element.find_element_by_name("selected[]").get_attribute("value")
-            groups.append(Group(name=text, id=id))
-        return groups
+        if self.group_cache is None:
+            wd = self.app.wd
+            self.open_groups_page()
+            self.group_cache = []
+            for element in wd.find_elements_by_css_selector("span.group"):
+                text = element.text
+                id = element.find_element_by_name("selected[]").get_attribute("value")
+                self.group_cache.append(Group(name=text, id=id))
+        return list(self.group_cache)
--- test/test_add_group.py
+++ test/test_add_group.py
@@ -9,7 +9,7 @@ def test_add_group(app):
         group = Group(name="gdfhjtyr", header="werthnm", footer="etthjyure")
         app.group.create(group)
         app.group.return_to_groups_page()
+        assert len(old_groups) + 1 == app.group.count()
         new_groups = app.group.get_group_list()
-        assert len(old_groups) + 1 == len(new_groups)
         old_groups.append(group)
         assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
