1455b922e9a56d0ee604aef2bead165e740026fd
aquamuerta <aquamuerta@gmail.com>
2018-12-26 13:00:09 +0300

    Добавлен случайный выбор элементов в тестах удаления и модификации групп.

--- fixture/group.py
+++ fixture/group.py
@@ -41,10 +41,13 @@ class GroupHelper:
         if not (wd.current_url.endswith("/group.php") and len(wd.find_elements_by_name("new")) > 0):
             wd.find_element_by_link_text("groups").click()
 
-    def modify_first_group(self, new_group_data):
+    def modify_first_group(self):
+        self.modify_group_by_index(0)
+
+    def modify_group_by_index(self, index, new_group_data):
         wd = self.app.wd
         self.open_groups_page()
-        self.select_first_group()
+        self.select_group_by_index(index)
         # open modification form
         wd.find_element_by_name("edit").click()
         # fill group form
@@ -54,18 +57,21 @@ class GroupHelper:
         self.group_cache = None
 
     def delete_first_group(self):
+        self.delete_group_by_index(0)
+
+    def delete_group_by_index(self, index):
         wd = self.app.wd
         self.open_groups_page()
-        self.select_first_group()
+        self.select_group_by_index(index)
         # submit deletion
         wd.find_element_by_name("delete").click()
         wd.find_element_by_link_text("group page").click()
         self.group_cache = None
 
-    def select_first_group(self):
+    def select_group_by_index(self, index):
         wd = self.app.wd
         # select first group
-        wd.find_element_by_name("selected[]").click()
+        wd.find_elements_by_name("selected[]")[index].click()
 
     def count(self):
         wd = self.app.wd
